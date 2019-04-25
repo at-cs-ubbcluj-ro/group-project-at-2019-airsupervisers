67e22c07e14cb7e7a6b5db7a3b0ed531a0f087e5
simonaburlacu <simonaburlacu01@gmail.com>
2019-04-25 21:40:55 +0300

    no bad words

--- ServerApp/src/main/java/Main.java
+++ ServerApp/src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
     }
 
     private static void test(){
-        String fmm = "192.168.100.4";
-        InetSocketAddress inetSockAddress2 = new InetSocketAddress(fmm, 3000);
+        String ipAddress= "192.168.100.4";
+        InetSocketAddress inetSockAddress2 = new InetSocketAddress(ipAddress, 3000);
         MyWebSocketServer wsServer2 = new MyWebSocketServer(inetSockAddress2);
         wsServer2.start();
     }

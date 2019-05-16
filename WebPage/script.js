b72bbfaeeeae7f5a828c9f5e367e7e6c1fda0ed7
Liana Elisabeta <lianaelisabeta.ln@gmail.com>
2019-05-16 09:51:33 +0300

    Add WebPage documentation

---
+++ WebPage/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    $("#linkList li").on('click',function(target){
+        $("#linkList li").removeClass("active");
+
+        $(target.currentTarget).addClass("active");
+    })
+});
